3e7996887b71c20cd5de3290fc23d7904aaceaad
Modibo Sanogo <modisalhydro@gmail.com>
2020-08-09 21:12:59 -0400

    Configuration is finished

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t msanogo/multi-client:latest  msanogo/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t msanogo/multi-server:latest msanogo/multi-server:$SHA-f ./server/Dockerfile  ./server
+docker build -t msanogo/multi-worker:latest msanogo/multi-worker:$SHA -f ./worker/Dockerfile  ./worker
+docker push msanogo/multi-client:latest
+docker push msanogo/multi-server:latest
+docker push msanogo/multi-worker:latest
+docker push msanogo/multi-client:$SHA
+docker push msanogo/multi-server:$SHA
+docker push msanogo/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=msanogo/multi-server:$SHA
+kubectl set image deployments/client-deployment client=msanogo/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=msanogo/multi-worker:$SHA
